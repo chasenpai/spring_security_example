e7cc89e95e7da124fc0f74c5b17ad41defb9d36d
junwvwv <wvwv0612@gmail.com>
2023-05-14 21:48:02 +0900

    OncePerRequestFilter 상속 예제

--- security-example/security-example/src/main/java/com/securityexample/filter/AuthenticationLoggingFilter.java
+++ security-example/security-example/src/main/java/com/securityexample/filter/AuthenticationLoggingFilter.java
@@ -4,20 +4,26 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Slf4j
-public class AuthenticationLoggingFilter implements Filter {
+public class AuthenticationLoggingFilter extends OncePerRequestFilter {
+
+    /**
+     * OncePerRequestFilter
+     * - 인증 정보 확인, 로깅 등 중복 실행이 되면 안되는 요청에 대해 한번만 실행되도록 보장해준다
+     * - HTTP 요청만 지원한다
+     * - shouldNotFilter 메소드를 재정의하여 필터가 특정 요청엔 적용 되지 않도록 할 수 있다
+     */
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
-        String requestToken = httpRequest.getHeader("request-token");
-
+        String requestToken = request.getHeader("request-token");
         log.info("authentication success! {}", requestToken);
-        chain.doFilter(request, response);
+        filterChain.doFilter(request, response);
     }
+
 }
